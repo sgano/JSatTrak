866fe68d42ed523af5003360b91f2518a5136de9
Shawn E. Gano <shawn@gano.name>
2015-10-16 22:42:39 -0500

    v4.1.8 - updated for Java 1.7/8  Fixes issue where saved files with custom satellites would not open (required adding and modifying the swingx source code). (16 Oct 2015)

--- src/jsattrak/gui/JSatTrak.java
+++ src/jsattrak/gui/JSatTrak.java
@@ -98,7 +98,7 @@ import name.gano.file.SaveImageFile;
  */
 public class JSatTrak extends javax.swing.JFrame implements InternalFrameListener, WindowListener, Serializable
 {
-    private String versionString = "Version 4.1.7  (27 Oct 2014)"; // Version of app
+    private String versionString = "Version 4.1.8  (16 Oct 2015)"; // Version of app
     
     // hastable to store all the statelites currently being processed
     private Hashtable<String,AbstractSatellite> satHash = new Hashtable<String,AbstractSatellite>();
