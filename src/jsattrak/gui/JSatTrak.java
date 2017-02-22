b6167962ed2ffbe6d4e196c54c38e5b01d94b155
Shawn E. Gano <shawn@gano.name>
2017-02-21 22:23:29 -0600

    Version 4.1.11 -- accepted push request from jonathanpallant to add  Iridium NEXT TLE data set.

--- src/jsattrak/gui/JSatTrak.java
+++ src/jsattrak/gui/JSatTrak.java
@@ -98,7 +98,7 @@ import name.gano.file.SaveImageFile;
  */
 public class JSatTrak extends javax.swing.JFrame implements InternalFrameListener, WindowListener, Serializable
 {
-    private String versionString = "Version 4.1.10  (18 April 2016)"; // Version of app
+    private String versionString = "Version 4.1.11  (21 Feb 2017)"; // Version of app
     
     // hastable to store all the statelites currently being processed
     private Hashtable<String,AbstractSatellite> satHash = new Hashtable<String,AbstractSatellite>();
