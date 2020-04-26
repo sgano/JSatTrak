a54c12302af8dd9d0d4887d2de7628b3d61c9e8d
Jose Damico <jose.damico@scicrop.com>
2020-04-26 17:02:47 -0300

    added basic maven and eclipse support

--- Required_Libraries/Beanshell/Readme_note.txt
+++
@@ -1,2 +0,0 @@
-// NOTE: in beanshell jar (i.e. bsh-2.0b4.jar) need to replace desktop.bsh, with one included with this source -- it allows bean shell desktop to be reopened
-- Shawn Gano, April 2008
---
+++ src/main/java/com/jdamico/jsattrak_eclipse/App.java
@@ -0,0 +1,13 @@
+package com.jdamico.jsattrak_eclipse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
