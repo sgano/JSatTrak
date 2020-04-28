0ec248f618411a14fb3e0756bcc29f915d0a1964
Jose Damico <jose.damico@scicrop.com>
2020-04-28 20:16:35 -0300

    added pom parent, and package support

--- jsattrak-eclipse/src/main/java/jsattrak/gui/GuiConstants.java
+++ jsattrak-eclipse/src/main/java/jsattrak/gui/GuiConstants.java
@@ -3,7 +3,7 @@ package jsattrak.gui;
 public interface GuiConstants {
 
 	public static final String APP_NAME = "JSatTrak";
-	public static final String APP_VERSION = "4.2.3 (Eclipse)";
+	public static final String APP_VERSION = "4.2.4 (Eclipse)";
 	public static final String APP_TITLE = "Java Satellite Tracker";
 	
 }
